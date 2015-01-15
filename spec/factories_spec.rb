4dc3dc9998b057b97cde3b37a54fc20b3b059aae
Richard Davis <rdavis.bacs@gmail.com>
2015-01-15 18:18:38 -0500

    valid factory

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "Factories" do
   FactoryGirl.factories.map(&:name).each do |factory|
-    it "The #{factory} is valid" do
+    it "has a valid #{factory} factory" do
       expect(FactoryGirl.build(factory)).to be_valid
     end
   end
